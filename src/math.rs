563c72251c56153b615fc0bad4b7db6846a5adda
Ed Kilgore <drakonix.dev@gmail.com>
2026-07-10 17:10:11 -0400

    update math types

--- src/math.rs
+++ src/math.rs
@@ -1,33 +1,121 @@
+macro_rules! wrap_glam {
+    ($name:ident, $inner:ty) => {
+        #[repr(transparent)]
+        #[derive(Clone, Copy, Debug, PartialEq)]
+        pub struct $name($inner);
+
+        impl std::ops::Deref for $name {
+            type Target = $inner;
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl std::ops::DerefMut for $name {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.0
+            }
+        }
+
+        impl From<$inner> for $name {
+            fn from(v: $inner) -> Self {
+                Self(v)
+            }
+        }
+
+        impl From<$name> for $inner {
+            fn from(v: $name) -> Self {
+                v.0
+            }
+        }
+
+        impl std::ops::Add for $name {
+            type Output = $name;
+            fn add(self, rhs: $name) -> $name {
+                $name(self.0 + rhs.0)
+            }
+        }
+
+        impl std::ops::Sub for $name {
+            type Output = $name;
+            fn sub(self, rhs: $name) -> $name {
+                $name(self.0 - rhs.0)
+            }
+        }
+
+        impl std::ops::Mul<f32> for $name {
+            type Output = $name;
+            fn mul(self, rhs: f32) -> $name {
+                $name(self.0 * rhs)
+            }
+        }
+    };
+}
+
+wrap_glam!(Vec2, glam::Vec2);
+wrap_glam!(Vec3, glam::Vec3);
+wrap_glam!(Quat, glam::Quat);
+
+impl Vec2 {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self(glam::Vec2::new(x, y))
+    }
+}
+
+impl Default for Vec2 {
+    fn default() -> Self {
+        Self(glam::Vec2::ZERO)
+    }
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self(glam::Vec3::new(x, y, z))
+    }
+}
+
+impl Default for Vec3 {
+    fn default() -> Self {
+        Self(glam::Vec3::ZERO)
+    }
+}
+
+impl Default for Quat {
+    fn default() -> Self {
+        Self(glam::Quat::IDENTITY)
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct Transform2D {
-    pub position: [f32; 2],
+    pub position: Vec2,
     pub rotation: f32,
-    pub scale: [f32; 2],
+    pub scale: Vec2,
 }
 
 impl Default for Transform2D {
     fn default() -> Self {
         Self {
-            position: [0.0, 0.0],
+            position: Vec2::default(),
             rotation: 0.0,
-            scale: [1.0, 1.0],
+            scale: Vec2::new(1.0, 1.0),
         }
     }
 }
 
 #[derive(Clone, Copy, Debug)]
 pub struct Transform3D {
-    pub position: [f32; 3],
-    pub rotation: [f32; 4],
-    pub scale: [f32; 3],
+    pub position: Vec3,
+    pub rotation: Quat,
+    pub scale: Vec3,
 }
 
 impl Default for Transform3D {
     fn default() -> Self {
         Self {
-            position: [0.0, 0.0, 0.0],
-            rotation: [0.0, 0.0, 0.0, 1.0],
-            scale: [1.0, 1.0, 1.0],
+            position: Vec3::default(),
+            rotation: Quat::default(),
+            scale: Vec3::new(1.0, 1.0, 1.0),
         }
     }
 }
